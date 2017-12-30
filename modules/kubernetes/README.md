67bcc23898ef2dfc207b38fb920c3ca9c38abb7d
Jonathan Pigrée <jonathan.pigree@gmail.com>
2017-12-30 22:47:27 +0100

    Adds custom kubernetes module

---
+++ modules/kubernetes/README.md
@@ -0,0 +1,20 @@
+# `Kubernetes` Aliases and Utilities
+
+Enhances `Kubernetes` command line integration by providing aliases,
+functions and by exposing repository status information to prompts.
+
+## Aliases
+
+- `kb` is an alias for `kubectl`
+- `kba` is an alias for `ubectl apply`
+- `kbc` is an alias for `kubectl create`
+- `kbD` is an alias for `kubectl delete`
+- `kbd` is an alias for `kubectl describe`
+- `kbe` is an alias for `kubectl exec`
+- `kbf` is an alias for `kubectl port-forward`
+- `kbg` is an alias for `kubectl get`
+- `kbl` is an alias for `kubectl logs`
+- `kblf` is an alias for `kubectl logs --follow`.
+- `kbr` is an alias for `kubectl run`
+
+[1]: https://kubernetes.io/
---
+++ modules/kubernetes/init.zsh
@@ -0,0 +1,40 @@
+#
+# Provides 'kubectl' aliases and utiities.
+#
+# Authors:
+#   Bruno Miguel Custodio <brunomcustodio@gmail.com>
+#
+
+# Return if requirements are not found.
+if (( ! ${+commands[kubectl]} )); then
+  return 1
+fi
+
+# Enable completion for 'kubectl'.
+cache_file="${0:h}/cache.zsh"
+kubectl_command="${commands[kubectl]}"
+
+if [[ "${kubectl_command}" -nt "${cache_file}" || ! -s "${cache_file}" ]]; then
+  ${kubectl_command} completion zsh >! "${cache_file}" 2> /dev/null
+fi
+
+source "${cache_file}"
+unset cache_file kubectl_command
+
+#
+# Aliases
+#
+
+alias kb='kubectl'
+
+alias kba='kubectl apply'
+alias kbc='kubectl create'
+alias kbD='kubectl delete'
+alias kbd='kubectl describe'
+alias kbe='kubectl exec'
+alias kbf='kubectl port-forward'
+alias kbg='kubectl get'
+alias kbl='kubectl logs'
+alias kblf='kubectl logs --follow'
+alias kbr='kubectl run'
+
